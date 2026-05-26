59fd86d6da3d1e4064b014c15e3fd86ad0b6ed36
jonassim <jonassim23@gmail.com>
2026-05-26 20:54:08 +0800

    feat:优化字符串工具

--- service-core/src/main/java/com/tech/util/StringUtil.java
+++ service-core/src/main/java/com/tech/util/StringUtil.java
@@ -19,13 +19,13 @@ import java.util.List;
  * @version 1.0
  * @since 2025-01-07
  */
-public class StrUtil {
+public class StringUtil {
 
     public static List<String> split(String str) {
         if (StringUtils.isBlank(str)) {
             return Collections.emptyList();
         }
-        Splitter split = Splitter.on(',').trimResults().omitEmptyStrings(); // 去前后空格&&去空string
+        Splitter split = Splitter.on(',').trimResults().omitEmptyStrings();
         return split.splitToList(str);
     }
 
@@ -36,40 +36,27 @@ public class StrUtil {
         return Joiner.on(",").join(list);
     }
 
-    /**
-     * 去掉括号里的文本
-     *
-     * @param str 文本
-     * @return 文本
-     */
     public static String removeBracketsContent(String str) {
-        // 使用正则表达式匹配中文括号（）和英文括号()
         return str.replaceAll("（.*?）|\\(.*?\\)", "");
     }
 
-    /**
-     * URL编码
-     * 使用UTF-8字符集按照RFC3986规则编码请求参数和参数取值。
-     */
     public static String percentEncode(String value) {
         return value != null ? URLEncoder.encode(value, StandardCharsets.UTF_8).replace("+", "%20")
                 .replace("*", "%2A").replace("%7E", "~") : null;
     }
 
-    public static String convertFenToYuan(int fen) {
+    public static String fenToYuan(int fen) {
         BigDecimal yuan = BigDecimal.valueOf(fen)
                 .divide(BigDecimal.valueOf(100), 2, RoundingMode.DOWN);
         return yuan.toPlainString();
     }
 
-    public static String convertFenToYuan2(int fen) {
+    public static String fenToYuanCompact(int fen) {
         BigDecimal yuan = BigDecimal.valueOf(fen)
                 .divide(BigDecimal.valueOf(100), 2, RoundingMode.DOWN);
 
-        // 转换为字符串并检查是否为整数
         String result = yuan.toPlainString();
         if (result.endsWith(".00")) {
-            // 去除末尾的 ".00"
             result = result.substring(0, result.length() - 3);
         }
         return result;
