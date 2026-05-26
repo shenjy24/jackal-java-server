9ac2c18321d39c515e917d9fb41557532f79c7e9
jonas <jonassim23@gmail.com>
2026-05-26 15:55:57 +0800

    chore:补充注释

--- service-core/src/main/java/com/tech/config/thread/AsyncExecutorConfig.java
+++ service-core/src/main/java/com/tech/config/thread/AsyncExecutorConfig.java
@@ -45,12 +45,12 @@ public class AsyncExecutorConfig implements AsyncConfigurer {
     private Executor bizExecutor;
 
     /**
-     * scheduled 注解默认线程池
+     * scheduled 注解自定义线程池
      */
     @Bean
     public TaskScheduler taskScheduler() {
         ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
-        taskScheduler.setPoolSize(10);
+        taskScheduler.setPoolSize(10);  // 设置线程池大小
         taskScheduler.setThreadNamePrefix("scheduled-");
         return taskScheduler;
     }
@@ -86,10 +86,22 @@ public class AsyncExecutorConfig implements AsyncConfigurer {
     private ThreadPoolTaskExecutor createExecutor(String prefix, int coreSize, int maxSize, int queueCapacity) {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(coreSize);
+        // 线程池维护线程的最大数量,只有在缓冲队列满了之后才会申请超过核心线程数的线程
         executor.setMaxPoolSize(maxSize);
+        // 缓存队列
         executor.setQueueCapacity(queueCapacity);
+        // 允许的空闲时间,当超过了核心线程出之外的线程在空闲时间到达之后会被销毁
         executor.setKeepAliveSeconds(120);
+        // 异步方法内部线程名称前缀
         executor.setThreadNamePrefix(prefix);
+        /*
+         * 当线程池的任务缓存队列已满并且线程池中的线程数目达到maximumPoolSize，如果还有任务到来就会采取任务拒绝策略
+         * 通常有以下四种策略：
+         * ThreadPoolExecutor.AbortPolicy: 丢弃任务并抛出RejectedExecutionException异常。
+         * ThreadPoolExecutor.DiscardPolicy：也是丢弃任务，但是不抛出异常。
+         * ThreadPoolExecutor.DiscardOldestPolicy：丢弃队列最前面的任务，然后重新尝试执行任务（重复此过程）
+         * ThreadPoolExecutor.CallerRunsPolicy：直接让提交任务的那个线程亲自去执行这个任务的 run() 方法
+         */
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.initialize();
         return executor;
